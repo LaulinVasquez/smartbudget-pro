16834509ab43f0ae217e3e336f76cc357b0b6a9c
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-06-14 20:04:51 -0600

    Configuer EJS and static asset middleware

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ import express from "express"
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
+// Middleware (setting up parse url-encoded allows Express to receive and process POST data)
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// static assets
+app.use(express.static("src/public"));
+
+// View engine
+app.set("view engine", "ejs")
+
+// Views location
+app.set("views", "./src/views")
+
+
+//  Home route
 app.get("/", (req, res) => {
-  res.send("SmartBudget Pro is under development 🚀")
-})
+  res.render("index", {
+    title: "Home"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port http://127.0.0.1:${PORT}`)
