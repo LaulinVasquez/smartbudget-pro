984c0db399030962561396e51d19f0d7d0f0d78a
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-06-26 16:54:55 -0600

    testing

--- src/database/migrations/001_users.sql
+++ src/database/migrations/001_users.sql
@@ -4,10 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(75) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) NOT NULL
-        CHECK (role IN ('user', 'advisor', 'admin')),
-    created_at TIMESTAMPTZ NOT NULL
-        DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL
-        DEFAULT CURRENT_TIMESTAMP
+    role VARCHAR(20) NOT NULL CHECK (role IN ('user', 'advisor', 'admin')),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
