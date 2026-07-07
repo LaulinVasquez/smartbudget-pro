faab178b5f302c698611a2f852343c918b98cb53
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-07-07 00:14:33 -0600

    optimization of styling and scripts

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -1,5 +1,41 @@
+const setHeadAssetsFunctionality = (res) => {
+  res.locals.styles = [];
+  res.locals.scripts = [];
+
+  res.addStyles = (css, priority = 0) =>{
+    res.locals.styles.push({ content: css, priority });
+  };
+  res.addScripts = (js, priority = 0) => {
+    res.locals.scripts.push({content: js, priority});
+  };
+
+  res.locals.renderStyles = () => {
+    return (
+        res.locals.styles
+        // Sort css by priority: higher numbers load first
+        .sort((a, b) => b.priority - a.priority)
+        .map((item) => item.content)
+        .join("\n")
+    );
+  };
+
+  res.locals.renderScripts = () => {
+    return (
+      res.locals.scripts    
+      // Sort scripts by priority: higher numbers load first
+        .sort((a, b) => b.priority - a.priority)
+        .map((item) => item.content)
+        .join("\n")
+    )
+  }
+};
+
 // This is where we add all local variables that we want to make available in all templates
 const addLocalVariables = (req, res, next) => {
+  setHeadAssetsFunctionality(res);
+  res.addScripts('<script src="../public/js/flash.js" defer></script>', 10);
+  res.addStyles('<link rel="stylesheet" href="/css/styles.css">', 10);
+
   // Logged-in state
   res.locals.isLoggedIn = false;
   // Current authenticated user (null if not logged in)
